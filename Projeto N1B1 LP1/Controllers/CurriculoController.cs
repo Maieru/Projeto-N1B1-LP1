daa8479dec4f9fca1affe721eaac5ddedd09c15a
Maieru Joao <soljoaomaieru@hotmail.com>
2022-03-19 19:50:42 -0300

    Adição de tela de index para curriculos

---
+++ Projeto N1B1 LP1/Controllers/CurriculoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Projeto_N1B1_LP1.DAO;
+using System;
+
+namespace Projeto_N1B1_LP1.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                var curriculoDAO = new CurriculoDAO();
+                var model = curriculoDAO.SelectAll();
+
+                return View("Index", model);
+            }
+            catch (Exception erro)
+            {
+                return View("Error", erro);
+            }
+        }
+    }
+}
