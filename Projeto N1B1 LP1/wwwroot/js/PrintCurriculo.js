53249e8c378d50eb3dd11be6c8bdfaf4f0117e8a
Maieru Joao <soljoaomaieru@hotmail.com>
2022-03-20 18:42:21 -0300

    Adição de sistema para impressão de curriculos

---
+++ Projeto N1B1 LP1/wwwroot/js/PrintCurriculo.js
@@ -0,0 +1,10 @@
+﻿function Print() {
+    var restorepage = $('body').html();
+    var printcontent = $('#Curriculo').clone();
+    var htmlOriginal = printcontent.html();
+    htmlOriginal += '<link href="/css/VisualizarCurriculo.css" rel="stylesheet" />';
+    printcontent.html(htmlOriginal);
+    $('body').empty().html(printcontent);
+    window.print();
+    $('body').html(restorepage);
+}
